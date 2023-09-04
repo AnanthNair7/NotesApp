71119ad4fbfe5b3fb6b975a30b2ebe58db3d2943
Ananth Nair <ananthnair96@gmail.com>
2023-09-04 15:55:51 +0530

    Completed UserDefault

---
+++ NotesAppApple/Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  DataModel.swift
+//  NotesAppApple
+//
+//  Created by Ananth Nair on 03/09/23.
+//
+
+import Foundation
+
+class Item : Codable {
+    var title : String = ""
+    var done : Bool = false
+}
